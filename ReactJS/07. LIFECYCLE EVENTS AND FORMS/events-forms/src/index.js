5f2282abaf8cdc584e32ae5162089e67d9903356
Georgi Borisov <gbbozhilov@abv.bg>
2019-03-07 02:31:20 +0200

    Remove unnecessary folder.

--- ReactJS/07. LIFECYCLE EVENTS AND FORMS/events-forms/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import General from "./components/General";
-import App from "./App";
-
-ReactDOM.render(
-    <App/>,
-    document.getElementById('root')
-);
--- ReactJS/08. EXERCISE LIFECYCLE EVENTS AND FORMS/games/src/App/AppFooter.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import SimpleSnackbar from './Snackbar';
-
-const AppFooter = (props) => <SimpleSnackbar
-    showSnack={props.showSnack}
-    message={props.message}
-/>;
-
-export default AppFooter;
