cbd32b3c3c03646318b982e163d91bfff1ec1d3f
Georgi Borisov <gbbozhilov@abv.bg>
2019-03-08 10:04:03 +0200

    Message here.

--- ReactJS Fundamentals/12. EXERCISE HIGHER-ORDER COMPONENTS/higher-order-components-ex/src/components/BindingForm/BindingForm.js
+++ ReactJS Fundamentals/12. EXERCISE HIGHER-ORDER COMPONENTS/higher-order-components-ex/src/components/BindingForm/BindingForm.js
@@ -1,10 +1,15 @@
 import React, {Component} from 'react';
 
 class BindingForm extends Component {
+    handleChange = (e) => {
+        console.log(`${e.target.name} => ${e.target.value}`);
+    };
+
     render() {
         return (
             <div>
                 <h1>Binding Form</h1>
+                {this.props.children}
             </div>
         );
     }
